f8f987ad7b0f40a0edafdd5bfe6bf5d20d265805
Stacy Cashmore <33732891+StacyCash@users.noreply.github.com>
2024-09-05 12:50:22 +0000

    Run through clean up

--- custom-element-demo/src/main.ce.ts
+++
@@ -1,9 +0,0 @@
-import { defineCustomElement } from 'vue'
-import StarWarsPlanets from './components/star-wars-planets.ce.vue'
-import StarWarsPerson from './components/star-wars-people.ce.vue'
-
-const planetsCE = defineCustomElement(StarWarsPlanets)
-const personCE = defineCustomElement(StarWarsPerson)
-
-customElements.define('starwars-planets', planetsCE)
-customElements.define('starwars-person', personCE)
